008ca64ede7bf731b2b72fa757d971f6d47c6382
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-04-14 22:04:45 +0100

    class contructor

--- OOP Projects/counter/app.js
+++ OOP Projects/counter/app.js
@@ -5,3 +5,23 @@ function getElement(selection) {
   }
   throw new Error(`Please check ${selection} selector, no such element exists.`);
 }
+
+class Counter {
+  constructor(element, value) {
+    this.element = element;
+    this.value = value;
+    this.resetBtn = element.querySelector(".reset");
+    this.increaseBtn = element.querySelector(".increase");
+    this.decreaseBtn = element.querySelector(".decrease");
+    this.valueDOM = element.querySelector(".value");
+    this.valueDOM.textContent = this.value;
+  }
+
+  reset() {
+    this.value = 0;
+    this.valueDOM.textContent = this.value;
+  }
+}
+
+const firstCounter = new Counter(getElement(".first-counter"), 100);
+const secondCounter = new Counter(getElement(".second-counter"), 200);
