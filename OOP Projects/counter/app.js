300cee528f9349a26b0fadb077d3dcb9e4a81402
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-04-14 21:31:39 +0100

    counter project functionality using function constructor

---
+++ OOP Projects/counter/app.js
@@ -0,0 +1,36 @@
+// Function constructor
+function Counter(element, value) {
+  this.counter = element;
+  this.value = value;
+  this.resetBtn = element.querySelector(".reset");
+  this.increaseBtn = element.querySelector(".increase");
+  this.decreaseBtn = element.querySelector(".decrease");
+  this.valueDOM = element.querySelector(".value");
+  this.valueDOM.textContent = this.value;
+}
+
+Counter.prototype.increase = function () {
+  this.value++;
+  this.valueDOM.textContent = this.value;
+};
+Counter.prototype.decrease = function () {
+  this.value++;
+  this.valueDOM.textContent = this.value;
+};
+Counter.prototype.reset = function () {
+  this.value++;
+  this.valueDOM.textContent = this.value;
+};
+
+const firstCounter = new Counter(getElement(".first-counter"), 100);
+const secondCounter = new Counter(getElement(".second-counter"), 200);
+
+function getElement(selection) {
+  const element = document.querySelector(selection);
+  if (element) {
+    return element;
+  }
+  throw new Error(`Please check ${selection} selector, no such element exists.`);
+}
+
+firstCounter.increaseBtn.addEventListener("click", this.increase);
