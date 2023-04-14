459d77086f1ce30597dd71e9e71ca827fd796ccf
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-04-14 21:56:07 +0100

    class contructor

---
+++ OOP Projects/counter/app.js
@@ -0,0 +1,7 @@
+function getElement(selection) {
+  const element = document.querySelector(selection);
+  if (element) {
+    return element;
+  }
+  throw new Error(`Please check ${selection} selector, no such element exists.`);
+}
