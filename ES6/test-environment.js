8ee0d5e8e23f9901c077905535a5d080e44ecc03
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-04-16 18:07:31 +0100

    ES6 syntax

---
+++ ES6/test-environment.js
@@ -0,0 +1,12 @@
+// Swap variables
+let first = "Aniebiet";
+let second = "Susan";
+
+// Method 1
+// let temp = first;
+// first = second;
+// second = temp;
+
+[first, second] = [second, first];
+
+console.log(first, second);
